8448382fda522c72362efd3ed01a9f46a5ee3d29
kykruz <k.kruzan@yahoo.com>
2024-04-17 11:34:41 -0500

    GA 3 Command

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -4,9 +4,16 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Command class", function() {
-
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+// test 1
+  test ("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
-
+// test 2
+  test ("constructor sets command type" , function() {
+    expect(new Command(!'') === true).toBe(false);
+  });
+  // test 3
+  test ("constructor sets a value passed in as the 2nd argument" , function() {
+    expect(new Command("commandType",!'') === true).toBe(false);
+  });
 });
